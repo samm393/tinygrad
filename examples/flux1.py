c84b98d6b72e89b817d47b8256b040a6bb0b4e52
samm393 <martin.samarthur@gmail.com>
2024-09-06 17:54:20 +0000

    JIT and dtype fixes

--- examples/flux1.py
+++ examples/flux1.py
@@ -620,125 +620,114 @@ class Util:
     return ae
 
 # https://github.com/black-forest-labs/flux/blob/main/src/flux/sampling.py
-class Sampling:
-  def get_noise(
-      num_samples: int,
-      height: int,
-      width: int,
-      dtype: str,
-      seed: int,
-  ):
-    Tensor.manual_seed(seed)
+def get_noise(
+    num_samples: int,
+    height: int,
+    width: int,
+    dtype: str,
+    seed: int,
+):
+  Tensor.manual_seed(seed)
 
-    return Tensor.randn(num_samples, 16, 2 * math.ceil(height / 16), 2 * math.ceil(width / 16), dtype=dtype)
+  return Tensor.randn(num_samples, 16, 2 * math.ceil(height / 16), 2 * math.ceil(width / 16), dtype=dtype)
 
-  def prepare(T5, clip, img: Tensor, prompt: str | list[str]) -> dict[str, Tensor]:
-    bs, _, h, w = img.shape
-    if bs == 1 and not isinstance(prompt, str):
-      bs = len(prompt)
+def prepare(T5, clip, img: Tensor, prompt: str | list[str]) -> dict[str, Tensor]:
+  bs, _, h, w = img.shape
+  if bs == 1 and not isinstance(prompt, str):
+    bs = len(prompt)
 
-    img = img.rearrange("b c (h ph) (w pw) -> b (h w) (c ph pw)", ph=2, pw=2)
-    if img.shape[0] == 1 and bs > 1:
-      img = img.expand((bs, *img.shape[1:]))
+  img = img.rearrange("b c (h ph) (w pw) -> b (h w) (c ph pw)", ph=2, pw=2)
+  if img.shape[0] == 1 and bs > 1:
+    img = img.expand((bs, *img.shape[1:]))
 
-    img_ids = Tensor.zeros(h // 2, w // 2, 3).contiguous()
-    img_ids[..., 1] = img_ids[..., 1] + Tensor.arange(h // 2)[:, None]
-    img_ids[..., 2] = img_ids[..., 2] + Tensor.arange(w // 2)[None, :]
-    img_ids = img_ids.rearrange("h w c -> 1 (h w) c")
-    img_ids = img_ids.expand((bs, *img_ids.shape[1:]))
+  img_ids = Tensor.zeros(h // 2, w // 2, 3).contiguous()
+  img_ids[..., 1] = img_ids[..., 1] + Tensor.arange(h // 2)[:, None]
+  img_ids[..., 2] = img_ids[..., 2] + Tensor.arange(w // 2)[None, :]
+  img_ids = img_ids.rearrange("h w c -> 1 (h w) c")
+  img_ids = img_ids.expand((bs, *img_ids.shape[1:]))
 
-    if isinstance(prompt, str):
-      prompt = [prompt]
-    txt = T5(prompt)
-    if txt.shape[0] == 1 and bs > 1:
-      txt = txt.expand((bs, *txt.shape[1:]))
-    txt_ids = Tensor.zeros(bs, txt.shape[1], 3)
+  if isinstance(prompt, str):
+    prompt = [prompt]
+  txt = T5(prompt)
+  if txt.shape[0] == 1 and bs > 1:
+    txt = txt.expand((bs, *txt.shape[1:]))
+  txt_ids = Tensor.zeros(bs, txt.shape[1], 3)
 
-    vec = clip(prompt)
-    if vec.shape[0] == 1 and bs > 1:
-      vec = vec.expand((bs, *vec.shape[1:]))
+  vec = clip(prompt)
+  if vec.shape[0] == 1 and bs > 1:
+    vec = vec.expand((bs, *vec.shape[1:]))
 
-    return {
-        "img": img,
-        "img_ids": img_ids.to(img.device).cast(img.dtype),
-        "txt": txt.to(img.device),
-        "txt_ids": txt_ids.to(img.device).cast(img.dtype),
-        "vec": vec.to(img.device),
-    }
+  return {
+      "img": img,
+      "img_ids": img_ids.to(img.device).cast(img.dtype),
+      "txt": txt.to(img.device),
+      "txt_ids": txt_ids.to(img.device).cast(img.dtype),
+      "vec": vec.to(img.device),
+  }
 
-  def time_shift(mu: float, sigma: float, t: Tensor):
-    return math.exp(mu) / (math.exp(mu) + (1 / t - 1) ** sigma)
+def time_shift(mu: float, sigma: float, t: Tensor):
+  return math.exp(mu) / (math.exp(mu) + (1 / t - 1) ** sigma)
 
-  def get_lin_function(
-      x1: float = 256, y1: float = 0.5, x2: float = 4096, y2: float = 1.15
-  ) -> Callable[[float], float]:
-    m = (y2 - y1) / (x2 - x1)
-    b = y1 - m * x1
-    return lambda x: m * x + b
+def get_lin_function(
+    x1: float = 256, y1: float = 0.5, x2: float = 4096, y2: float = 1.15
+) -> Callable[[float], float]:
+  m = (y2 - y1) / (x2 - x1)
+  b = y1 - m * x1
+  return lambda x: m * x + b
 
-  def get_schedule(
-      num_steps: int,
-      image_seq_len: int,
-      base_shift: float = 0.5,
-      max_shift: float = 1.15,
-      shift: bool = True,
-  ) -> list[float]:
-    # extra step for zero
-    step_size = -1.0 / num_steps
-    timesteps = Tensor.arange(1, 0 + step_size, step_size)
+def get_schedule(
+    num_steps: int,
+    image_seq_len: int,
+    base_shift: float = 0.5,
+    max_shift: float = 1.15,
+    shift: bool = True,
+) -> list[float]:
+  # extra step for zero
+  step_size = -1.0 / num_steps
+  timesteps = Tensor.arange(1, 0 + step_size, step_size)
 
-    # shifting the schedule to favor high timesteps for higher signal images
-    if shift:
-      # estimate mu based on linear estimation between two points
-      mu = Sampling.get_lin_function(y1=base_shift, y2=max_shift)(image_seq_len)
-      timesteps = Sampling.time_shift(mu, 1.0, timesteps)
+  # shifting the schedule to favor high timesteps for higher signal images
+  if shift:
+    # estimate mu based on linear estimation between two points
+    mu = get_lin_function(y1=base_shift, y2=max_shift)(image_seq_len)
+    timesteps = time_shift(mu, 1.0, timesteps)
 
-    return timesteps.tolist()
-
-  def denoise(
-      model: Model.Flux,
-      # model input
-      img: Tensor,
-      img_ids: Tensor,
-      txt: Tensor,
-      txt_ids: Tensor,
-      vec: Tensor,
-      # sampling parameters
-      timesteps: list[float],
-      guidance: float = 4.0,
-  ):
-    # this is ignored for schnell
-    guidance_vec = Tensor.full((img.shape[0],), guidance, device=img.device, dtype=img.dtype)
-    for t_curr, t_prev in tqdm(list(zip(timesteps[:-1], timesteps[1:])), f"Denoising"):
-      t_vec = Tensor.full((img.shape[0],), t_curr, dtype=img.dtype, device=img.device)
-      pred = step(model, img, t_vec.contiguous(), img_ids, txt, txt_ids.contiguous(), vec, guidance_vec.contiguous())
-      img = img + (t_prev - t_curr) * pred
-
-    return img
-
-  def unpack(x: Tensor, height: int, width: int) -> Tensor:
-    return x.rearrange(
-        "b (h w) (c ph pw) -> b c (h ph) (w pw)",
-        h=math.ceil(height / 16),
-        w=math.ceil(width / 16),
-        ph=2,
-        pw=2,
-    )
+  return timesteps.tolist()
 
 @TinyJit
-def step(model, img, t_vec, img_ids, txt, txt_ids, vec, guidance_vec):
-  pred = model(
-      img=img,
-      img_ids=img_ids,
-      txt=txt,
-      txt_ids=txt_ids,
-      y=vec,
-      timesteps=t_vec,
-      guidance=guidance_vec,
+def run(model, *args): return model(*args).realize()
+
+def denoise(
+    model: Model.Flux,
+    # model input
+    img: Tensor,
+    img_ids: Tensor,
+    txt: Tensor,
+    txt_ids: Tensor,
+    vec: Tensor,
+    # sampling parameters
+    timesteps: list[float],
+    guidance: float = 4.0,
+):
+  # this is ignored for schnell
+  guidance_vec = Tensor((guidance,), device=img.device, dtype=img.dtype).expand((img.shape[0],))
+  for t_curr, t_prev in tqdm(list(zip(timesteps[:-1], timesteps[1:])), f"Denoising"):
+    t_vec = Tensor((t_curr,), device=img.device, dtype=img.dtype).expand((img.shape[0],))
+    pred = run(model, img, img_ids, txt, txt_ids, t_vec, vec, guidance_vec)
+    img = img + (t_prev - t_curr) * pred
+
+  return img
+
+def unpack(x: Tensor, height: int, width: int) -> Tensor:
+  return x.rearrange(
+      "b (h w) (c ph pw) -> b c (h ph) (w pw)",
+      h=math.ceil(height / 16),
+      w=math.ceil(width / 16),
+      ph=2,
+      pw=2,
   )
 
-  
-  return pred.realize()
+
 
 # https://github.com/black-forest-labs/flux/blob/main/src/flux/cli.py
 @dataclass
@@ -800,7 +789,7 @@ if __name__ == "__main__":
     t0 = time.perf_counter()
 
     # prepare input
-    x = Sampling.get_noise(
+    x = get_noise(
         1,
         opts.height,
         opts.width,
@@ -813,9 +802,9 @@ if __name__ == "__main__":
     clip = Util.load_clip(args.name)
 
     # embed text to get inputs for model
-    inp = Sampling.prepare(T5, clip, x, prompt=opts.prompt)
+    inp = prepare(T5, clip, x, prompt=opts.prompt)
     for k, v in inp.items():  v.realize()
-    timesteps = Sampling.get_schedule(opts.num_steps, inp["img"].shape[1], shift=(args.name != "flux-schnell"))
+    timesteps = get_schedule(opts.num_steps, inp["img"].shape[1], shift=(args.name != "flux-schnell"))
 
     # done with text embedders
     del T5, clip
@@ -824,16 +813,16 @@ if __name__ == "__main__":
     model = Util.load_flow_model(args.name)
 
     # denoise initial noise
-    x = Sampling.denoise(model, **inp, timesteps=timesteps, guidance=opts.guidance)
+    x = denoise(model, **inp, timesteps=timesteps, guidance=opts.guidance)
 
     # done with model
-    del model, step
+    del model, run
 
     # load autoencoder
     ae = Util.load_ae(args.name)
 
     # decode latents to pixel space
-    x = Sampling.unpack(x.float(), opts.height, opts.width).realize()
+    x = unpack(x.float(), opts.height, opts.width).realize()
     x = ae.decode(x).realize()
 
   t1 = time.perf_counter()
